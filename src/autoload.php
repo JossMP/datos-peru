6737d94781996203316d9acfe19d566f41e1285f
Josue Mazco Puma <JossMP@gmail.com>
2018-09-24 07:39:05 -0500

    v2.0.0

--- src/autoload.php
+++ src/autoload.php
@@ -3,5 +3,5 @@
 	require (__DIR__ . "/curl.php");
 	require (__DIR__ . "/essalud/essalud.php");
 	require (__DIR__ . "/mintra/mintra.php");
-	require (__DIR__ . "/reniec/reniec.php");
+	//require (__DIR__ . "/reniec/reniec.php");
 ?>
